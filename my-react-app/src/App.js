bc818f9f376a74c0ae04eb138cb5b7ecb80b10b7
MagdaSlifierz <m.slifierz@gmail.com>
2023-11-06 23:32:11 -0800

    frontend

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,17 +1,29 @@
+import {
+  Center,
+  Text,
+  Heading,
+  VStack,
+  Button,
+  Input,
+  HStack,
+  Container,
+  SimpleGrid,
+  Box,
+  Image,
+  Spinner,
+} from "@chakra-ui/react";
 
-import { Center, Heading, VStack, Text, HStack, Button, SimpleGrid, Image} from "@chakra-ui/react" ;
+import { useState, useEffect } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import { useEffect, useState} from "react"
+import { upload } from "@testing-library/user-event/dist/upload";
 
-
-export default function App(){
-  const [allPhotos, setAllPhotos] = useState([]);
+function App() {
+  const [isSelected, setIsSelected] = useState(false);
   const [selectedFile, setSelectedFile] = useState(null);
+  const [allPhotos, setAllPhotos] = useState([]);
+  const [uploadSuccessful, setUploadSuccessful] = useState(false);
+  const [showSpinner, setShowSpinner] = useState(false);
 
-
-  const onInputChange = e => {
-
-  }
   useEffect(() => {
     fetch("http://127.0.0.1:8000/photos")
       .then((response) => response.json())
@@ -19,42 +31,80 @@ export default function App(){
         console.log(data);
         setAllPhotos(data);
       });
-  }, []);
+  }, [uploadSuccessful]);
 
+  const onInputChange = (e) => {
+    console.log(e.target.files[0]);
+    setIsSelected(true);
+
+    setSelectedFile(e.target.files[0]);
+  };
+  const onButtonClick = (e) => {
+    console.log("Button clicked..");
+    e.target.value = "";
+  };
+
+  const onFileUpload = (e) => {
+    setShowSpinner(true);
+    const formData = new FormData();
+    formData.append("file", selectedFile, selectedFile.name);
+    fetch("http://127.0.0.1:8000/photos", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success posting!!");
+        setUploadSuccessful(!uploadSuccessful);
+        setShowSpinner(false);
+      });
+  };
   return (
     <ChakraProvider>
-    <Center bg="black" color="white" padding={8}>
-      <VStack spacing={7}>
-        <Heading>
-          Funny Animal Pictures
-        </Heading>
-        <Text>
-        Smile every day and look at the photos.
-        </Text>
-        <HStack>
-          <input type="file" onChange={onInputChange} onClick={onFileUpload}></input>
-          <Button size= "lg" colorScheme="red" isDisabled={null} onClick={null} Upload Picture ></Button>
-        </HStack>
+      <Center bg="black" color="white" padding={8}>
+        <VStack spacing={7}>
+          <Heading>Funny Pictures</Heading>
+          <Text>Smile every day and look at the photos!</Text>
+          <HStack>
+            <input
+              type="file"
+              onChange={onInputChange}
+              onClick={onButtonClick}
+            ></input>
 
-        <Heading>
-          Your photos
-        </Heading>
-        <SimpleGrid columns={3} spacing={8}>
-          {allPhotos.map((photo) => {
-            return (
-              <Image
-                borderRadius={25}
-                boxSize="300px"
-                src={photo["photo_url"]}
-                fallbackSrc="https://via.placeholder.com/150"
-                objectFit="cover"/>
-            );
-          })}
-
-        </SimpleGrid>
-      </VStack> 
-
-    </Center>
+            <Button
+              size="lg"
+              colorScheme="red"
+              isDisabled={!isSelected}
+              onClick={onFileUpload}
+            >
+              Upload Photo
+            </Button>
+            {showSpinner && (
+              <Center>
+                <Spinner size="xl" />
+              </Center>
+            )}
+          </HStack>
+          <Heading>Your Photos</Heading>
+          <SimpleGrid columns={3} spacing={8}>
+            {allPhotos.length !== 0 &&
+              allPhotos.map((photo) => {
+                return (
+                  <Image
+                    borderRadius={25}
+                    boxSize="300px"
+                    src={photo["photo_url"]}
+                    fallbackSrc="https://via.placeholder.com/150"
+                    objectFit="cover"
+                  />
+                );
+              })}
+          </SimpleGrid>
+        </VStack>
+      </Center>
     </ChakraProvider>
-  )
+  );
 }
+
+export default App;
